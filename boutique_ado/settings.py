bc030bb3db32f79e3ca310c4fb3dd17d339256bf
oludare89 <azizoyekan@hotmail.com>
2021-07-15 19:58:15 +0000

    added add to bag functionality

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -73,7 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # require by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'bags.contexts.bag_contents',
+                'bag.contexts.bag_contents',
             ],
         },
     },
